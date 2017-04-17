9e45edfc4e33b74c07d7f6fcffdab0df5fc4f757
Guillaume <anakhon@gmail.com>
2017-04-17 16:09:21 +0200

    Working example

--- README.md
+++ README.md
@@ -1,1 +1,91 @@
-# flask-esipy-example
+# Flask + EsiPy Example
+
+This project is a small ready to work example on how to use EsiPy with Flask.
+
+The following libraries are used:
+* Flask _for the webapp part_
+* [EsiPy](https://github.com/Kyria/EsiPy) _for the ESI API_
+* [Flask-Login](https://flask-login.readthedocs.io/en/latest/) _for the login / session process_
+* [Flask-SQLAlchemy](http://flask-sqlalchemy.pocoo.org) and [SQLAlchemy](https://www.sqlalchemy.org/) _for the database part_
+* [Flask-Migrate](https://flask-migrate.readthedocs.io/en/latest/) _for the migration commands_
+
+You will also require:
+* __Virtualenv__ to setup virtualenv
+* Depending on the database you want to use, the required libraries (by default this example uses SQLite, which is embedded in Python)
+
+__Also, this example is made with Python 2.7, it might works with Python 3, but you may have to correct some lines of code__
+
+## Get and init the project
+
+1. Clone the repository on your system
+```shell
+git clone https://github.com/Kyria/flask-esipy-example.git
+```
+
+2. Create a virtualenv
+```shell
+cd flask-esipy-example
+virtualenv venv
+source venv/bin/activate
+```
+
+3. Install requirements
+```shell
+pip install -r requirements.txt
+```
+
+4. Setup the FLASK_APP environment variable
+```shell
+export FLASK_APP=app
+```
+
+
+## Create your app in https://developers.eveonline.com
+
+1. Go to https://developers.eveonline.com
+2. Login and go to `manage applications`
+3. Create a new application
+4. Fill all the fields
+
+__Requirements:__
+* For the scope, you will need `esi-wallet.read_character_wallet.v1` for this example
+* The callback URL must be `http://<SOME_IP_OR_DOMAIN>:<SOME_PORT>/sso/callback`
+
+
+## APP Configuration
+
+1. Copy and rename the `config.dist` in `config.py`
+2. Edit it.
+
+```python
+PORT = <SOME_PORT>
+HOST = '<SOME_IP_OR_DOMAIN>'
+
+SQLALCHEMY_DATABASE_URI = 'sqlite:///app.db'  # this is your database connection informations http://docs.sqlalchemy.org/en/latest/core/engines.html#database-urls
+
+# Fill these lines with the data you get from https://developers.eveonline.com
+ESI_SECRET_KEY = ''  # your secret key
+ESI_CLIENT_ID = ''  # your client ID
+```
+
+## How to start everything
+#### First, you will need to run the migration to create the required user table
+```shell
+flask db upgrade
+```
+> INFO  [alembic.runtime.migration] Context impl SQLiteImpl.<br>
+> INFO  [alembic.runtime.migration] Will assume non-transactional DDL.<br>
+> INFO  [alembic.runtime.migration] Running upgrade  -> fab636b98bc7, create_user_table
+
+
+#### Run the app
+```shell
+python app.py
+```
+> * Restarting with stat<br>
+> * Debugger is active!<br>
+> * Debugger PIN: 206-933-487<br>
+> * Running on http://localhost:5015/ (Press CTRL+C to quit)
+
+And now you can connect to [http://localhost:5015/](http://localhost:5015/) to see it working (if you kept default configs)
+
---
+++ app.py
@@ -0,0 +1,227 @@
+# -*- encoding: utf-8 -*-
+from datetime import datetime
+
+from esipy import App
+from esipy import EsiClient
+from esipy import EsiSecurity
+from esipy.exceptions import APIException
+
+from flask import Flask
+from flask import flash
+from flask import g
+from flask import redirect
+from flask import render_template
+from flask import request
+from flask import session
+from flask import url_for
+
+from flask_login import LoginManager
+from flask_login import UserMixin
+from flask_login import current_user
+from flask_login import login_required
+from flask_login import login_user
+from flask_login import logout_user
+
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm.exc import NoResultFound
+
+import config
+import logging
+import time
+
+# logger stuff
+logger = logging.getLogger('flask-esipy-example')
+formatter = logging.Formatter(
+    '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+)
+console = logging.StreamHandler()
+console.setLevel(logging.DEBUG)
+console.setFormatter(formatter)
+logger.addHandler(console)
+
+# init app and load conf
+app = Flask(__name__)
+app.config.from_object(config)
+
+# init db
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+# init flask login
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'sso.login'
+
+
+# -----------------------------------------------------------------------
+# Database models
+# -----------------------------------------------------------------------
+class User(db.Model, UserMixin):
+    # our ID is the character ID from EVE API
+    character_id = db.Column(
+        db.BigInteger,
+        primary_key=True,
+        autoincrement=False
+    )
+    character_owner_hash = db.Column(db.String(255))
+    character_name = db.Column(db.String(200))
+
+    # SSO Token stuff
+    access_token = db.Column(db.String(100))
+    access_token_expires = db.Column(db.DateTime())
+    refresh_token = db.Column(db.String(100))
+
+    def get_id(self):
+        """ Required for flask-login """
+        return self.character_id
+
+    def get_sso_data(self):
+        """ Little "helper" function to get formated data for esipy security
+        """
+        return {
+            'access_token': self.access_token,
+            'refresh_token': self.refresh_token,
+            'expires_in': (
+                self.access_token_expires - datetime.utcnow()
+            ).total_seconds()
+        }
+
+    def update_token(self, token_response):
+        """ helper function to update token data from SSO response """
+        self.access_token = token_response['access_token']
+        self.access_token_expires = datetime.fromtimestamp(
+            time.time() + token_response['expires_in'],
+        )
+        if 'refresh_token' in token_response:
+            self.refresh_token = token_response['refresh_token']
+
+
+# -----------------------------------------------------------------------
+# Flask Login requirements
+# -----------------------------------------------------------------------
+@login_manager.user_loader
+def load_user(character_id):
+    """ Required user loader for Flask-Login """
+    return User.query.get(character_id)
+
+
+# -----------------------------------------------------------------------
+# ESIPY Init
+# -----------------------------------------------------------------------
+# create the app
+esiapp = App.create(config.ESI_SWAGGER_JSON)
+
+# init the security object
+esisecurity = EsiSecurity(
+    app=esiapp,
+    redirect_uri=config.ESI_CALLBACK,
+    client_id=config.ESI_CLIENT_ID,
+    secret_key=config.ESI_SECRET_KEY,
+)
+
+# init the client
+esiclient = EsiClient(
+    security=esisecurity,
+    cache=None,
+    headers={'User-Agent': config.ESI_USER_AGENT}
+)
+
+
+# -----------------------------------------------------------------------
+# Login / Logout Routes
+# -----------------------------------------------------------------------
+@app.route('/sso/login')
+def login():
+    """ this redirects the user to the EVE SSO login """
+    return redirect(esisecurity.get_auth_uri(
+        scopes=['esi-wallet.read_character_wallet.v1']
+    ))
+
+
+@app.route('/sso/logout')
+@login_required
+def logout():
+    logout_user()
+    flash("You have been logged out successfully.", 'info')
+    return redirect(url_for("index"))
+
+
+@app.route('/sso/callback')
+def callback():
+    """ This is where the user comes after he logged in SSO """
+    # get the code from the login process
+    code = request.args.get('code')
+
+    # now we try to get tokens
+    try:
+        auth_response = esisecurity.auth(code)
+    except APIException as e:
+        return 'Login EVE Online SSO failed: %s' % e, 403
+
+    # we get the character informations
+    cdata = esisecurity.verify()
+
+    # if the user is already authed, we log him out
+    if current_user.is_authenticated:
+        logout_user()
+
+    # now we check in database, if the user exists
+    # actually we'd have to also check with character_owner_hash, to be
+    # sure the owner is still the same, but that's an example only...
+    try:
+        user = User.query.filter(
+            User.character_id == cdata['CharacterID'],
+        ).one()
+
+    except NoResultFound:
+        user = User()
+        user.character_id = cdata['CharacterID']
+
+    user.character_owner_hash = cdata['CharacterOwnerHash']
+    user.character_name = cdata['CharacterName']
+    user.update_token(auth_response)
+
+    # now the user is ready, so update/create it
+    try:
+        db.session.merge(user)
+        db.session.commit()
+
+        login_user(user)
+        session.permanent = True
+        flash('You have successfully logged in.', 'success')
+
+    except:
+        logger.exception("Cannot login the user - uid: %d" % user.character_id)
+        db.session.rollback()
+        logout_user()
+        flash('Something went wrong. Please try to login again', 'error')
+
+    return redirect(url_for("index"))
+
+
+# -----------------------------------------------------------------------
+# Index Routes
+# -----------------------------------------------------------------------
+@app.route('/')
+def index():
+    wallet = {}
+
+    # if the user is authed, get the wallet content !
+    if current_user.is_authenticated:
+        # give the token data to esisecurity, it will check alone
+        # if the access token need some update
+        esisecurity.update_token(current_user.get_sso_data())
+
+        op = esiapp.op['get_characters_character_id_wallets'](
+            character_id=current_user.character_id
+        )
+        wallet = esiclient.request(op)
+
+    return render_template('base.html', **{
+        'wallet': wallet,
+    })
+
+
+if __name__ == '__main__':
+    app.run(port=config.PORT, host=config.HOST)
