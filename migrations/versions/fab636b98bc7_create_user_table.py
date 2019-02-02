a59b03bd278e6e76da53d1f69a89b7b0e93ce922
Guillaume <anakhon@gmail.com>
2019-02-03 00:53:17 +0100

    Changed token size to 4096.
    If you are using sqlite as the example, you'll need to drop the table and recreate it. Else, just run `flask db migrate -m "update token size"` and `flask db upgrade`

--- migrations/versions/fab636b98bc7_create_user_table.py
+++ migrations/versions/fab636b98bc7_create_user_table.py
@@ -1,7 +1,7 @@
 """create_user_table
 
 Revision ID: fab636b98bc7
-Revises: 
+Revises:
 Create Date: 2017-04-17 14:55:22.352000
 
 """
@@ -22,7 +22,7 @@ def upgrade():
     sa.Column('character_id', sa.BigInteger(), autoincrement=False, nullable=False),
     sa.Column('character_owner_hash', sa.String(length=255), nullable=True),
     sa.Column('character_name', sa.String(length=200), nullable=True),
-    sa.Column('access_token', sa.String(length=100), nullable=True),
+    sa.Column('access_token', sa.String(length=4096), nullable=True),
     sa.Column('access_token_expires', sa.DateTime(), nullable=True),
     sa.Column('refresh_token', sa.String(length=100), nullable=True),
     sa.PrimaryKeyConstraint('character_id')
